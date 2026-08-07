963f0af302c16485b87999f56b9c2d1bddc19c2f
AMITABHA7430 <amitbhag17@gmail.com>
2026-08-08 01:15:58 +0530

    first

---
+++ src/main/java/com/sih/govassist/GovassistApplication.java
@@ -0,0 +1,13 @@
+package com.sih.govassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovassistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sih/govassist/GovassistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sih.govassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
